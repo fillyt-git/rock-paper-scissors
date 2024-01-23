d69a6973683abefb60c1b4920181086cae6a741f
fillyt-git <156151778+fillyt-git@users.noreply.github.com>
2024-01-22 22:02:40 -0600

    I did a bunch again.
    I did not commit early and often lol.

---
+++ rps-script.js
@@ -0,0 +1,53 @@
+
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        }  
+}
+
+let computerSelection = getComputerChoice().toLowerCase()
+let playerSelection = prompt("Choice").toLowerCase()
+
+for (; (playerSelection !== ("rock" || "paper" || "scissors")); ) {
+    playerSelection = prompt("Error, choose again.").toLowerCase();
+}
+
+function play(playerSelection, computerSelection) {
+    switch (playerSelection + computerSelection) {
+        case "paperrock":
+        case "rockscissors":
+        case "scissorspaper":
+            return (
+                `You Win! ${playerSelection.toUpperCase().charAt(0)}` + `${playerSelection.slice(1)}`+ 
+                ` beats ${computerSelection.toUpperCase().charAt(0)}` + `${computerSelection.slice(1)}!`
+            )
+        case "rockpaper":
+        case "scissorsrock":
+        case "paperscissors":
+            return (
+                `You Lose! ${computerSelection.toUpperCase().charAt(0)}` + `${computerSelection.slice(1)}` + 
+                ` beats ${playerSelection.toUpperCase().charAt(0)}` + `${playerSelection.slice(1)}!`
+            )
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            return "tie";
+    }
+}
+
+if (play(playerSelection, computerSelection) === "tie") {
+    console.log("Tie!");
+    computerSelection = getComputerChoice().toLowerCase();
+    playerSelection = prompt("Choose Again").toLowerCase();
+    for (; (playerSelection !== ("rock" || "paper" || "scissors")); ) {
+        playerSelection = prompt("Error, choose again.").toLowerCase();
+    };
+    play(playerSelection, computerSelection);
+}
+
+console.log(play(playerSelection, computerSelection))
